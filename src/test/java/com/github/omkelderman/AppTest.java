f6ef1227722603f3e7e1322677b2006307c84227
Olle Kelderman <omkelderman@gmail.com>
2015-09-28 23:57:46 +0200

    add test maven project

---
+++ src/test/java/com/github/omkelderman/AppTest.java
@@ -0,0 +1,16 @@
+package com.github.omkelderman;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void noTest() {
+        assertTrue(true);
+    }
+}
